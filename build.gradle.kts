4cf510f1dec2a4661be1079c63075334ba5834e7
lynox2624-ux <lynox2624@gmail.com>
2025-11-01 18:20:21 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    id("java")
+    id("fabric-loom") version "1.6-SNAPSHOT"
+    id("maven-publish")
 }
 
-group = "com.optiloaderproof"
 version = "1.0.0"
+group = "com.optiloaderproof"
 
 repositories {
     mavenCentral()
@@ -11,5 +12,22 @@ repositories {
 }
 
 dependencies {
-    implementation("net.fabricmc:fabric-loader:0.14.24")
+    minecraft("com.mojang:minecraft:1.21.4")
+    mappings("net.fabricmc:yarn:1.21.4+build.1:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.16.0")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    withSourcesJar()
+}
+
+tasks {
+    jar {
+        from("LICENSE") {
+            rename { "${it}_${project.name}" }
+        }
+    }
 }
